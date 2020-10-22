77b8706f7c71e1e74f59beababb742b7e20cf331
Gabriel Lima <gabriellima.silva96@gmail.com>
2020-10-22 13:29:57 -0300

    feat: enviando uma solicitação e preenchendo os dados na variável de processo

--- src/app/modules/solicitation/components/solicitation-form/solicitation-form.component.ts
+++ src/app/modules/solicitation/components/solicitation-form/solicitation-form.component.ts
@@ -1,5 +1,10 @@
 import { WfUser } from './../../../../core/workflow/model/user';
 import { WorkflowService } from './../../../../core/workflow/services/workflow.service';
+import {
+  WfProcessStep,
+  WorkflowCockpit,
+  WfFormData,
+} from './../../../../workflow-cockpit.d';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -13,6 +18,25 @@ export class SolicitationFormComponent implements OnInit {
   constructor(private workflowService: WorkflowService) {}
 
   ngOnInit(): void {
+    // Pegar todas as informações do usuário logado
     this.user = this.workflowService.getUser();
+
+    // Associar o método de envio com a função da plataforma
+    this.workflowService.onSubmit(this.onSubmit.bind(this));
+  }
+
+  onSubmit(info: WfProcessStep, dataPlataform: WorkflowCockpit) {
+    // Verificação das regras de négocio.
+    if (true) {
+      // Enviar dados para preencher as variáveis de processo da plataforma
+      return {
+        formData: {
+          nomeSolicitante: this.user.fullname,
+        },
+      };
+    } else {
+      // Cancelar o envio das informações para a plataforma
+      this.workflowService.abortSubmit();
+    }
   }
 }
